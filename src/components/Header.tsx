2befd50201d523a4e7aed4b721ec2e57ab4f24bd
MapYh <145336421+MapYh@users.noreply.github.com>
2024-05-14 00:05:46 +0200

    Added some styling to main title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,12 +24,12 @@ const Header = () => {
       <main className="main">
         <section className="main__side">
           <section className="main__side-text">
-            <h1 className="main__side-text-hone">test</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
-              harum, molestiae dolor at vel magnam aliquid officiis error,
-              facere possimus quasi dolores repellat doloribus quis facilis
-              dolorem. Sit, unde velit.
+            <h1 className="main__side-text-header-one">
+              Hi, My <br />
+              name is <b>Test</b>
+            </h1>
+            <p className="main__side-text-text-one">
+              I'm an <b>creative developer</b> from Southern, Sweden.
             </p>
           </section>
         </section>
