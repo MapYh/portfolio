2604824845cc7f54ef03c5d92305cdd65df2774e
MapYh <145336421+MapYh@users.noreply.github.com>
2024-05-14 00:26:07 +0200

    Added text and styling to main projects section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,19 @@ import "./Main.scss";
 const Main = () => {
   return (
     <main className="main__projects">
-      <article className="main__projects__header">Main</article>
+      <article className="main__projects__header">
+        <article className="main__projects__header-text">
+          <h2 className="main__projects__header-text--styling-header">
+            Let's work <br /> together
+          </h2>
+          <p className="main__projects__header-text--styling-paragraph">
+            From <b>API development and documentation</b>, <br />
+            <b>single-page apps</b> to something more bespoke with{" "}
+            <b>React JS</b>. I help awesome people to build ambitious yet
+            accessible web projects.
+          </p>
+        </article>
+      </article>
     </main>
   );
 };
