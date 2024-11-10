a9f3c6843ec00fa29d7648d0f6f786fe3202a0df
MapYh <145336421+MapYh@users.noreply.github.com>
2024-11-10 17:09:22 +0100

    I think i fixed the layout of the logo on the front page.

--- src/components/Header_main.tsx
+++ src/components/Header_main.tsx
@@ -17,7 +17,7 @@ const Header_main = () => {
         </section>
         <section className="main__side ">
           <section className="main__logo--styling">
-            <div className="first_triangle  "></div>
+            <div className="first_triangle"></div>
             <div className="second_triangle"></div>
           </section>
         </section>
