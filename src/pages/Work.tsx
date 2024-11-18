6b5176abbe02571045e570794adf2edda401b21b
MapYh <145336421+MapYh@users.noreply.github.com>
2024-11-18 18:36:09 +0100

    Added background color to the work page.

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -10,58 +10,60 @@ import background from "../assets/work-background.svg";
 const Work = () => {
   return (
     <>
-      <Header />
-      <section className="stack_and_image">
-        <section className="stack_and_image__stack ">
-          <h2 className="stack_and_image__stack--heading">
-            MERNing Ideas into Reality
-          </h2>
-          <p className="stack_and_image__stack--paragraph">
-            I develop
-            <b>interactive, accessible websites and products </b>
-            using the MERN stack (MongoDB, Express.js, React.js, and Node.js),
-            delivering scalable, high-performance solutions with seamless
-            frontend and backend integration.
-          </p>
-          <section className="break"></section>
-          <p className="stack_and_image__stack--paragraph">
-            I also leverage cloud <b>technologies</b> to design, deploy, and
-            manage reliable and <b>scalable applications</b>, ensuring optimal
-            performance and resource efficiency.
-          </p>
-        </section>
+      <section className="work-wrapper background_color_work ">
+        <Header />
+        <section className="stack_and_image">
+          <section className="stack_and_image__stack ">
+            <h2 className="stack_and_image__stack--heading">
+              MERNing Ideas into Reality
+            </h2>
+            <p className="stack_and_image__stack--paragraph">
+              I develop
+              <b>interactive, accessible websites and products </b>
+              using the MERN stack (MongoDB, Express.js, React.js, and Node.js),
+              delivering scalable, high-performance solutions with seamless
+              frontend and backend integration.
+            </p>
+            <section className="break"></section>
+            <p className="stack_and_image__stack--paragraph">
+              I also leverage cloud <b>technologies</b> to design, deploy, and
+              manage reliable and <b>scalable applications</b>, ensuring optimal
+              performance and resource efficiency.
+            </p>
+          </section>
 
-        <section className="stack_and_image__img">
-          <img
-            className="stack_and_image__img-background"
-            src={background}
-            alt=""
-          />
-          <img
-            className="stack_and_image__img--styling"
-            src={Mongo_icon}
-            alt=""
-          />
-          <img
-            className="stack_and_image__img--styling"
-            src={Express_icon}
-            alt=""
-          />
-          <img
-            className="stack_and_image__img--styling"
-            src={React_icon}
-            alt=""
-          />
-          <img
-            className="stack_and_image__img--styling"
-            src={nodejs_icon}
-            alt=""
-          />
-          <img
-            className="stack_and_image__img--styling"
-            src={aws_logo}
-            alt=""
-          />
+          <section className="stack_and_image__img">
+            <img
+              className="stack_and_image__img-background"
+              src={background}
+              alt=""
+            />
+            <img
+              className="stack_and_image__img--styling"
+              src={Mongo_icon}
+              alt=""
+            />
+            <img
+              className="stack_and_image__img--styling"
+              src={Express_icon}
+              alt=""
+            />
+            <img
+              className="stack_and_image__img--styling"
+              src={React_icon}
+              alt=""
+            />
+            <img
+              className="stack_and_image__img--styling"
+              src={nodejs_icon}
+              alt=""
+            />
+            <img
+              className="stack_and_image__img--styling"
+              src={aws_logo}
+              alt=""
+            />
+          </section>
         </section>
       </section>
     </>
